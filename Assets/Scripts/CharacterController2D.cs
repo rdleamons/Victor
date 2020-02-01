13666879a64e0b25de43f3076474a4971c3d411c
Unknown <rdleamons@gmail.com>
2020-02-01 16:17:55 -0600

    Character Movement
    Character sketch is in, player can move/jump appropriately. Still fixing collision.

---
+++ Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CharacterController2D : MonoBehaviour
+{
+    private float m_JumpForce = 400f;                          // Amount of force added when the player jumps. 
+    private LayerMask m_WhatIsGround;                          // A mask determining what is ground to the character
+    private Transform m_GroundCheck;                           // A position marking where to check if the player is grounded.
+    private Transform m_CeilingCheck;                          // A position marking where to check for ceilings
+
+    const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+    private bool m_Grounded;            // Whether or not the player is grounded.
+    const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
+    private Rigidbody2D m_Rigidbody2D;
+    private Vector2 velocity = Vector2.zero;
+
+    private void Awake()
+    {
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        m_Grounded = false;
+
+        // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+        // This can be done using layers instead but Sample Assets will not overwrite your project settings.
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+                m_Grounded = true;
+        }
+    }
+
+
+    public void Move(float move, bool jump)
+    {
+        // If the player should jump...
+        if (m_Grounded && jump)
+        {
+            // Add a vertical force to the player.
+            m_Grounded = false;
+            m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+        }
+    }
+}
